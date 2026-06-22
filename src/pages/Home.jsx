c2fee1455ddc367428ae301438506fe0fe7bce35
Praneeth <praneeth3366@192.168.1.10>
2026-06-22 11:06:19 +0530

    feat: remove Summer Camp PromoPopup component and rebuild website

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import Hero from '../components/Hero';
-import PromoPopup from '../components/PromoPopup';
 
 const Home = () => {
   return (
     <main>
-      <PromoPopup />
       <Hero />
     </main>
   );
